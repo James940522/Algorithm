77be364504a0289a9c2f8a803bbc61758044a48d
james940522 <James940522@gmail.com>
2021-11-18 21:27:41 +0900

    배열 clear

---
+++ Baekjoon/Array.js
@@ -0,0 +1,12 @@
+let fs = require("fs");
+let input = fs.readFileSync("test.txt").toString().trim().split("\n");
+
+input = input[0].split('')
+
+const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+const result = new Array(26).fill(0)
+
+input.forEach(el => result[alphabet.indexOf(el)]++)
+
+console.log(result.join(" "))
