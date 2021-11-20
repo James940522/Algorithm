be8d60e23c09e2af951c94cd3d2c399ce314ae7b
james940522 <James940522@gmail.com>
2021-11-20 20:21:56 +0900

    방_번호 완료

--- beakjoon/array/방_번호.js
+++ beakjoon/array/방_번호.js
@@ -1,2 +1,22 @@
 let fs = require("fs");
-let input = fs.readFileSync("test.txt").toString().trim().split("\n");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+input = `${input[0]}`
+
+const set = '0123456789'
+
+const result = new Array(10).fill(0)
+
+for (let i = 0; i < input.length; i++) {
+
+    result[set.indexOf(input[i])]++
+
+    if( i+1 === input.length ) {
+        result[6] = (result[6]/2)
+        result[9] = (result[9]/2)
+    }
+
+}
+
+console.log(Math.max(...result))
+
