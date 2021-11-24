042869c9843447949459b2f05fef1f8770b4a097
james940522 <James940522@gmail.com>
2021-11-24 16:19:00 +0900

    두수의합

--- beakjoon/array/두수의합.js
+++ beakjoon/array/두수의합.js
@@ -1,12 +1,54 @@
 let fs = require("fs");
 let input = fs.readFileSync("test.txt").toString().trim().split("\n");
 
-console.log(input)
+let count = 0;
 
-const max = 1000000;
+const len = input.shift();
 
-for (let i = 0; i < input.length; i++) {
+console.log(len)
 
+const result = input.pop()
+
+input = input[0].split(" ")
+
+const arr = input.map(el => Number(el)).sort((a, b) => a - b);
+
+console.log(arr)
+
+let left = 0;
+
+let right = arr.length-1;
+
+
+while ( left < right ) {
+
+    if ( arr[left] + arr[right] === Number(result) ) {
+        count ++;
+    } 
     
+    if ( arr[left] + arr[right] >= Number(result) ) {
+        right --;
+    }else if ( arr[left] + arr[right] < Number(result) ) {
+        left ++;
+    }
 
 }
+
+
+// for (let i = 0; i < len; i++) {
+
+//     // for (let j = 1; j < len; j++) {
+//         let j = i + 1;
+
+//         if(!input[j]) {
+//             break;
+//         }else if (Number(result) === input[i] + input[j]) {
+//             count++
+//         }
+
+//     // }
+
+// }
+
+
+console.log(count)
